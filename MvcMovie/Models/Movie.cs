e7f76b4ce611fbfd81691050f16295e8598abe6e
Sydney L <sll224@cornell.edu>
2020-07-30 16:44:09 -0400

    Add search by title and genre

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models
 {
@@ -11,9 +12,15 @@ namespace MvcMovie.Models
         public int Id { get; set; }
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Genre")]
         public string genere { get; set; }
+
+        [Display(Name = "Price")]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal price { get; set; }
 
     }
